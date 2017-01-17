a24b4a6458d5a7bd3978509b0ffab455a6b21911
Tu Tran <tu.tran@temple.edu>
2017-01-16 20:08:04 -0500

    Fix empty string problem; add presence

--- static/js/script.js
+++ static/js/script.js
@@ -10,32 +10,47 @@ $(document).ready(function () {
     const publishKey = "pub-c-851e2df6-cacf-487b-a0cc-39177adc1c03";
     const subscribeKey = "sub-c-18d97eee-dc04-11e6-93ca-0619f8945a4f";
 
+    // generate a unique user id
+    var userID = 'user' + (Math.floor(Math.random() * 10) + 1).toString();
+    console.log(userID);
+
     var pubnub = new PubNub({
         publishKey: publishKey,
-        subscribeKey: subscribeKey
+        subscribeKey: subscribeKey,
+        ssl: true,
+        uuid: userID
     })
 
     pubnub.addListener({
-        message: function (message) {
-            console.log(message.message);
-            $pad.val(message.message);
-            $markdown.html(converter.makeHtml(message.message));
+        message: function(obj) {
+            console.log(obj);
+            $pad.val(obj.message.such);
+            $markdown.html(converter.makeHtml(obj.message.such));
         },
-        status: function (status) {
+        status: function(status) {
             if (status.category == "PNConnectedCategory") {
                 console.log("Connected successfully");
             }
+        },
+        presence: function(event) {
+            console.log(event.action);
+            console.log(event.timestamp);
+            console.log(event.uuid);
+            console.log(event.occupancy);
         }
     });
 
     pubnub.subscribe({
-        channels: ['markdown']
+        channels: ['markdown'],
+        withPresence: true
     });
 
     var publishValue = function (text) {
         pubnub.publish(
             {
-                message: text,
+                message: {
+                    such: text
+                },
                 channel: 'markdown'
             },
             function (status, response) {
